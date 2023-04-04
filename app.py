14c625d830e1866b13006773e522e81e1af61d63
Eric-Josh <techjosmar@gmail.com>
2023-04-04 18:08:01 +0100

    HLS To Mp4

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from src.reqforwarder import forwarder_blueprint
 from src.autoSuggestKeyword import autosuggest_blueprint
 from src.converter import converter_blueprint
 from src.m3u8ToMp4 import m3u8ToMp4_blueprint
+from src.HLSToMp4 import HLSToMp4_blueprint
 
 app = Flask(__name__)
 # Register blueprint
@@ -14,6 +15,7 @@ app.register_blueprint(forwarder_blueprint)
 app.register_blueprint(autosuggest_blueprint)
 app.register_blueprint(converter_blueprint)
 app.register_blueprint(m3u8ToMp4_blueprint)
+app.register_blueprint(HLSToMp4_blueprint)
 
 @app.route('/')
 def home():
---
+++ src/HLSToMp4.py
@@ -0,0 +1,95 @@
+from flask import Blueprint, request, jsonify, make_response
+from flask_cors import CORS
+from os import path, stat
+from time import strftime, gmtime, sleep
+
+import subprocess
+import secrets
+import threading
+import threading
+import requests
+
+# Route setup
+HLSToMp4_blueprint = Blueprint('HLSToMp4_blueprint', __name__, url_prefix='/api')
+CORS(HLSToMp4_blueprint, resources=r'/api/*')
+
+# Init storage path
+basedir = path.abspath(path.dirname(__file__))
+targetPath = path.join(basedir, '..', 'static/mp4/')
+# targetPath = 'C:/Users/"BUYPC COMPUTER"/Documents/SupremeWeb/Projects/viloud/public/uploads'
+targetPathMain = '/home/tubetargeterapp/linkomaticapp.com/app/static/mp4/'
+
+def converter(data):
+    """
+    background Process handled by Threads
+    :return: None
+    """
+    print("Started Task ...")
+    print(threading.current_thread().name)
+    sleep(5)
+
+    command_1 = ["ffmpeg", "-i", data['link'], "-acodec", "copy", "-vcodec", "copy", data['target']]
+    command_2 = ["ffmpeg", "-i", data['link'], "-acodec", "copy", "-bsf:a", "aac_adtstoasc", "-vcodec",  "copy", data['target']]
+
+    if subprocess.run(command_1).returncode == 0:
+        print ("command_1 Script Ran Successfully...")
+    elif subprocess.run(command_2).returncode == 0:
+        print ("command_2 Script Ran Successfully...")
+
+    if path.exists(data['target']):
+        # Get video length
+        result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                             "format=duration", "-of",
+                             "default=noprint_wrappers=1:nokey=1", data['target']],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+
+        videoLength = round(float(result.stdout))
+        if videoLength >= 3600:
+            actualTime = strftime("%H:%M:%S", gmtime(videoLength))
+        else:
+            actualTime = strftime("%M:%S", gmtime(videoLength))
+        fileSize = stat(data['target']).st_size
+    else:
+        raise Exception('Unable to convert hls')
+
+    # post to webhook
+    requests.post(data['webhook'], data={
+        'duration': actualTime,
+        'durationInSec': videoLength,
+        'fileSize': fileSize,
+        'videoTitle': data['filename'],
+        'webhook': data['webhook'],
+        'user': data['user'],
+        'webhookReferer': data['webhookReferer'],
+        'jobOwner': data['jobOwner'],
+        'lhash': data['lhash'],
+        'channels': data['channels'],
+    })
+
+    print("Task completed .....")
+    
+    
+# Routes
+@HLSToMp4_blueprint.route('/hls/convert', methods=['POST']) 
+def toMP4():
+    name = secrets.token_hex(16)
+    filename = f"{name}.mp4" 
+
+    data = {
+        'link': request.json.get('link',''),
+        'webhook': request.json.get('webhook', ''),
+        'user': request.json.get('user', ''),
+        'jobOwner': request.json.get('jobOwner', ''),
+        'lhash': request.json.get('lhash', ''),
+        'channels': request.json.get('channels', ''),
+        'webhookReferer': request.json.get('webhookReferer', ''),
+        'target': targetPath+filename,
+        'filename': name
+    }
+    
+    threading.Thread(target=converter(data)).start()
+
+    return make_response(jsonify({
+        'message': 'Task is being processed'
+    }))
